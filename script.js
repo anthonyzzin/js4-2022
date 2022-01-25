af387d4fa9e4c70594e6cc8f6e5a3eec089b3572
Anthony Almeida <80277056+anthonyzzin@users.noreply.github.com>
2022-01-25 13:52:46 -0300

    4° Desafio em JS de 2022

---
+++ script.js
@@ -0,0 +1,28 @@
+function mens(){
+    console.log(`Quebrando a maldição do Olá Mundo :)`)
+}
+mens()
+
+function nome(name){
+    console.log(`${name}`)
+}
+nome(`Anthony`)
+
+function perfil(name, numb, song){
+    console.log(`${name}, ${numb}, ${song}`)
+}
+
+perfil(`Nathan`, `18`, `rap`)
+
+function perfil(film , music){
+    console.log(`${film} , ${music}`)
+}
+
+perfil(`Homem-Aranha: Sem Volta Pra Casa` , `Clint Eastwood`)
+
+function triple(num){
+    return num*3
+}
+triple(8)
+
+console.log(triple(8))
